ead1b229866f9744344d437452ab3a3447acda0c
JairoAdrianVR <jairoavegaromero@gmail.com>
2025-07-09 17:00:46 -0600

    Arrays - Clase 8/7/25

--- JAVASCRIPT/sintaxis.js
+++ JAVASCRIPT/sintaxis.js
@@ -105,3 +105,31 @@ funcioncita();
 // Fat Arrow Functions Funciones flecha
 const funcionFlecha = () => { console.log("Soy anonima") };
 funcionFlecha();
+
+//Callback ???? Una funcion que se pasa como parametro a otra
+
+function saludar(){
+    console.log("Holiwis");
+}
+
+
+function funcionQueUsaCallback(callbackfn){
+    let nombre = callbackfn();
+    
+    console.log("Soy la funcion que te ejecuta, saludos "+nombre);
+}
+
+
+
+/* 
+**  FUNCION QUE USA CALLBACK
+** NECESITA QUE LE RETORNES UN NOMBRE
+*/
+
+funcionQueUsaCallback(  () => { 
+    
+   return("Jairo");
+   
+}
+
+)
