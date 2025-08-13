b4abdac2f844113fce0ae3816e8f7b7e3a93d1bc
JairoAdrianVR <jairoavegaromero@gmail.com>
2025-08-13 01:27:40 -0600

    Estado - Clase 12/8/25

--- REACT/concepts-project/src/App.jsx
+++ REACT/concepts-project/src/App.jsx
@@ -10,10 +10,13 @@ function App() {
       <h1>Holiwis</h1>
 
       {/* Este es el primer componente jaja saludos */}
-      <Saludo nombre='Jairo' />
-      <Saludo nombre='Jose' />
-      <Saludo nombre='Jorge' />
-      <Saludo nombre='Mayerli' />
+      <Saludo 
+      nombre='Jairo' 
+      apellido="Vega" 
+      />
+      <Saludo nombre='Jose' apellido="Mendez" />
+      <Saludo nombre='Jorge' apellido="Galdamez"/>
+      <Saludo nombre='Mayerli' apellido="Martinez"/>
     </>
   )
 }
