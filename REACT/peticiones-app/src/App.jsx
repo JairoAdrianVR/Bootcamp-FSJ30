2258a27ab7059f8f453e56d620dd1211effe8ff9
JairoAdrianVR <jairoavegaromero@gmail.com>
2025-08-28 11:45:40 -0600

    React Hook Form - Clase 27/8/25

--- REACT/peticiones-app/src/App.jsx
+++ REACT/peticiones-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {ListCharacters } from './views/listCharacters/ListCharacters'
 import { FavoritesDataProvider } from './contexts/FavoritesContext'
 import {BrowserRouter, Route, Routes} from 'react-router'
+import { SessionView } from './views/session/SessionView'
 // Asincronismo -> manejar código que tarda en completarse como solicitudes a un servidor o lecturas de archivos, sin bloquear el resto del programa.
 // Promesa -> La esperanza de una posible respuesta a eso que va a tardar
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<ListCharacters />}/>
-
+          <Route path='/session' element={<SessionView />}/>
         </Routes>
       </BrowserRouter>
 
