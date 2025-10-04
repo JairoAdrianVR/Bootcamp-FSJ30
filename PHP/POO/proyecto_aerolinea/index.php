87e98c9df5e70b49f90ffb3dbef0fc6d21874191
JairoAdrianVR <jairoavegaromero@gmail.com>
2025-10-03 18:16:12 -0600

    POST PHP - Clase 30/9/25

--- PHP/POO/proyecto_aerolinea/index.php
+++ PHP/POO/proyecto_aerolinea/index.php
@@ -1,5 +1,31 @@
 <?php 
 print_r($_POST);
+
+//Llamamos la clase Aerolineas para crear un objeto
+// include -> Incluir el archivo y si no existe, mostrar un error y continuar la ejecucion del codigo
+// require -> Requerir el archivo y si no existe, mostrar un error y detener la ejecucion del codigo
+
+// include_once -> Incluir el archivo una sola vez, si se vuelve a llamar dentro de este archivo, va a usar la misma referencia
+// require_once -> Requerir el archivo una sola vez, si se vuelve a llamar dentro de este archivo, va a usar la misma referencia
+
+require_once './Aerolinea.php';
+
+//Persistencia de datos
+$aerolineas = [];
+
+if(isset($_POST['nombre_aerolinea'], $_POST['cantidad_aviones'], $_POST['tipo_aerolinea']) ){
+
+    $nombre = $_POST['nombre_aerolinea'];
+    $cant_aviones = $_POST['cantidad_aviones'];
+    $tipo_aero = $_POST['tipo_aerolinea'];
+    
+    $aerolineacita = new Aerolinea($nombre,$cant_aviones,$tipo_aero);
+    
+    print_r($aerolineacita);
+    array_push($aerolineas,$aerolineacita);
+    echo "<h1>Aerolineas hasta ahora</h1><br>"; 
+    print_r($aerolineas);
+}
 ?>
 
 <!DOCTYPE html>
