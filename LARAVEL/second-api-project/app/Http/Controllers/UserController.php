3b16455dfcd374364f1212412b06413841779842
JairoAdrianVR <jairoavegaromero@gmail.com>
2025-11-12 21:12:14 -0600

    Proyecto Laravel - Delete - New Project - Clase 12/11/25

---
+++ LARAVEL/second-api-project/app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //
+    public function register(Request $request){
+        // Validamos los datos del request o la peticion
+        $request->validate([
+            'name' => 'required|string|max:255|unique:users|alpha|min:8|regex:/^[A-Za-z]+$/',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+    }
+}
